49f6a95a7d511b475d5ad0f363ecb8661c416f65
Yeshmita <yeshmitachouuhan@gmail.com>
2026-05-23 14:30:06 +0530

    fix gitignore

--- betterme/settings.py
+++ betterme/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'betterme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'betterme_db',
+        'USER': 'root',
+        'PASSWORD': 'Yeshmita',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
