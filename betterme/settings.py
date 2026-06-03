fdf91c267ece6755029471be01b4921ae47dbb0c
Yeshmita <yeshmitachouuhan@gmail.com>
2026-06-03 23:25:21 +0530

    remove secrets from settings

--- betterme/settings.py
+++ betterme/settings.py
@@ -26,7 +26,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-6o06@i+c6*3i!8ku!4i#u)d53y@k6_!5cyar%ljk$tk6cir3p1' )
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
